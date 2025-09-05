e9f2b2485d4bfd3d11b08eec41992317b6b4d017
Rainier <rainiertabing21@gmail.com>
2025-09-05 11:02:32 -0400

    Added a discount method (question 33)

--- TicketMachine.java
+++ TicketMachine.java
@@ -83,4 +83,12 @@ public class TicketMachine
         // Clear the balance.
         balance = 0;
     }
+    
+    /**
+     * Reduce price by the given amount.
+     */
+    public void discount(int amount)
+    {
+        price = price - amount;
+    }
 }
